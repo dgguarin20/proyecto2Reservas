2edf83d5507636a47e77c8f87ce221301b5ed82f
dgguarin20 <dg.guarin20@uniandes.edu.co>
2018-09-17 07:06:11 -0500

    Update webpack.prod.js

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -20,12 +20,7 @@ module.exports = merge(commonConfig, {
       },
       output: {
         comments: false
-      },
-        devServer: {
-    contentBase: './client/public',
-    historyApiFallback: true,
-    stats: 'minimal' // none (or false), errors-only, minimal, normal (or true) and verbose
-  }
+      }
     })
   ]
 });
