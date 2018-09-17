1950d8cf9ace260184343bab5e18f9bc3e15d4f4
dav014 <dg.guarin20@uniandes.edu.co>
2018-09-17 06:48:55 -0500

    combinacion

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# proyecto2Reservas
-reservas
+# Reservas el campincito
+
+[![NPM Version](https://img.shields.io/npm/v/reactstrap.svg?branch=master)](https://www.npmjs.com/package/reactstrap)
+
+This is a boilerplate project using the following technologies:
+- [React](https://facebook.github.io/react/) and [React Router](https://reacttraining.com/react-router/) for the frontend
+- [Express](http://expressjs.com/) and [Mongoose](http://mongoosejs.com/) for the backend
+- [Sass](http://sass-lang.com/) for styles (using the SCSS syntax)
+- [Webpack](https://webpack.github.io/) for compilation
+
+
+## Requirements
+
+- [Node.js](https://nodejs.org/en/) 6+
+
+```shell
+npm install
+```
+
+
+## Running
+
+Make sure to add a `config.js` file in the `config` folder. See the example there for more details.
+
+##Warning cases
+in case there is a problem with bcrypt, then do the following:
+npm install -g node-gyp
+npm install --g --production windows-build-tools
+
+and then isntall
+npm install bcrypt
+
+or change it to bcryptjs
+
+Production mode:
+
+```shell
+npm start
+```
+
+Development (Webpack dev server) mode:
+
+```shell
+npm run start:dev
+```
---
+++ server/server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const fs = require('fs');
+const historyApiFallback = require('connect-history-api-fallback');
+const mongoose = require('mongoose');
+const path = require('path');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+
+const config = require('../config/config');
+const webpackConfig = require('../webpack.config');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const port  = process.env.PORT || 5000;
+
+
+const Uri = 'mongodb://creador:creador1@ds261072.mlab.com:61072/mongo';
+mongoose.connect(Uri)
+.then(db => console.log('Db is connected'))
+  .catch(error => console.error(error));
+// Configuration
+// ================================================================================================
+
+// Set up Mongoose
+
+mongoose.Promise = global.Promise;
+
+const app = express();
+
+app.get('/api/passwords', (req, res) => {
+  const count = 5;
+
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// API routes
+require('./routes')(app);
+
+if (isDev) {
+  const compiler = webpack(webpackConfig);
+
+  app.use(historyApiFallback({
+    verbose: false
+  }));
+
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    contentBase: path.resolve(__dirname, '../client/public'),
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  }));
+
+  app.use(webpackHotMiddleware(compiler));
+  app.use(express.static(path.resolve(__dirname, '../dist')));
+} else {
+  app.use(express.static(path.resolve(__dirname, '../dist')));
+  app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+    res.end();
+  });
+}
+
+app.listen(port, '0.0.0.0', (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.info('>>> ðŸŒŽ Open http://0.0.0.0:%s/ in your browser.', port);
+});
+
+module.exports = app;
+
